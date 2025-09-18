eb4b78951f42407f1c0384de4cd0a6e5fad2ad06
frenchm7 <frenchm7@hawaii.edu>
2025-09-18 01:53:54 -1000

    commit

---
+++ Lab4/Ex2.py
@@ -0,0 +1,15 @@
+# Define a list of survey responses values (5, 7, 3, 8) and store them 
+# in a variable. Define a tuple of response IDs (1012, 1035, 1021, 1053)
+# and add these to the list.
+#Name: Micah French
+#Date: 09/17/2025
+
+#response_values = [5, 7, 3, 8]
+#response_values.sort()
+#respondent_ids = (1012, 1035, 1021, 1053)
+#response_values.append(respondent_ids)
+#print("Combined response values and IDs:", response_values)
+
+response_values=[(1012, 5), (1035, 7), (1021, 3), (1053, 8)]
+response_values.sort()
+print("Combined response values and IDs:", response_values)
